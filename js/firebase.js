8e25bfc252c0ef70b7147e9a42229c833e654374
tirso0368-debug <tirso0368@gmail.com>
2026-02-26 22:56:51 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,19 @@
-//firebase config
+// 🔥 Firebase versión navegador (SIN NPM)
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSa3xZ2iKtvnPaXeNQMluaoa27iiOEyeM",
+  authDomain: "vortex-app-c8b00.firebaseapp.com",
+  projectId: "vortex-app-c8b00",
+  storageBucket: "vortex-app-c8b00.appspot.com",
+  messagingSenderId: "174144803653",
+  appId: "1:174144803653:web:eeb421bd5770ee1f69a42c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
